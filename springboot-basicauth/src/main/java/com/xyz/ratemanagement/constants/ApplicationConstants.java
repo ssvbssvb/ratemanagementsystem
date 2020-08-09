e055601975589986a91277454d27f836ceda34ab
ssvbssvb <ssvb.srinivas@gmail.com>
2020-08-09 13:17:14 +0400

    updated constants file

--- springboot-basicauth/src/main/java/com/xyz/ratemanagement/constants/ApplicationConstants.java
+++ springboot-basicauth/src/main/java/com/xyz/ratemanagement/constants/ApplicationConstants.java
@@ -4,4 +4,16 @@ public final class ApplicationConstants {
 	
 	public static final String VAT_URL = "https://surcharge.free.beeceptor.com/surcharge";
 	
+	
+	public static final String SEARCH_RATE = "/searchRate/{rateId}";
+	public static final String UPDATE_RATE = "/updateRate/{rateId}";
+	public static final String DELETE_RATE = "/deleteRate/{rateId}";
+	public static final String ADD_RATE = "/addRate";
+	
+	
+	
+	
+	
+	
+	
 }
--- springboot-basicauth/src/main/java/com/xyz/ratemanagement/controller/RateManagementController.java
+++ springboot-basicauth/src/main/java/com/xyz/ratemanagement/controller/RateManagementController.java
@@ -6,16 +6,15 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.xyz.ratemanagement.constants.ApplicationConstants;
 import com.xyz.ratemanagement.exception.ErrorMessages;
 import com.xyz.ratemanagement.model.Rate;
 import com.xyz.ratemanagement.service.RateManagentServiceImpl;
@@ -30,7 +29,7 @@ public class RateManagementController {
 	public RateManagentServiceImpl rateManagentService;
 
 	@SuppressWarnings("unchecked")
-	@GetMapping(path = "/searchRate/{rateId}")
+	@RequestMapping(value=ApplicationConstants.SEARCH_RATE, method=RequestMethod.GET,produces=MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<?> searchRate(@PathVariable("rateId") Long rateId) {
 
 		logger.info("Start:searchRate " + rateId);
@@ -50,7 +49,7 @@ public class RateManagementController {
 	}
 
 	@SuppressWarnings("unchecked")
-	@PutMapping("/updateRate/{rateId}")
+	@RequestMapping(value=ApplicationConstants.UPDATE_RATE, method=RequestMethod.PUT,produces=MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<?> updateRate(@PathVariable("rateId") Long rateId, @RequestBody Rate rates) {
 
 		logger.info("Start:updateRate " + rateId);
@@ -71,7 +70,7 @@ public class RateManagementController {
 	}
 
 	@SuppressWarnings("unchecked")
-	@DeleteMapping("/deleteRate/{rateId}")
+	@RequestMapping(value=ApplicationConstants.DELETE_RATE, method=RequestMethod.DELETE)
 	public ResponseEntity<Void> deleteRate(@PathVariable("rateId") Long rateId) throws Exception {
 
 		logger.info("Start:deleteRate " + rateId);
@@ -92,10 +91,12 @@ public class RateManagementController {
 		return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
 	}
 
-	@PostMapping("/addRate")
+	
+	@RequestMapping(value=ApplicationConstants.ADD_RATE, method=RequestMethod.POST,produces=MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<Rate> addRate(@Valid @RequestBody Rate rates) {
 		logger.info("Start:addRate ");
 		try {
+			
 			rateManagentService.save(rates);
 		} catch (Exception e) {
 			logger.error("Error in addRate :" + e.getMessage());
